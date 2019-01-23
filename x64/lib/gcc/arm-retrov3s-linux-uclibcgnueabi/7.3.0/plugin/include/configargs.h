d79c3fcd7fd125802d353cdb1a29794294eae1e5
steward-fu <steward.fu@gmail.com>
2019-01-23 14:23:53 +0800

    new build for retro-v3s handheld

---
+++ x64/lib/gcc/arm-retrov3s-linux-uclibcgnueabi/7.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./configure --prefix=/opt/retro-v3s --sysconfdir=/opt/retro-v3s/etc --enable-static --target=arm-retrov3s-linux-uclibcgnueabi --with-sysroot=/opt/retro-v3s/arm-retrov3s-linux-uclibcgnueabi/sysroot --disable-__cxa_atexit --with-gnu-ld --disable-libssp --disable-multilib --with-gmp=/opt/retro-v3s --with-mpc=/opt/retro-v3s --with-mpfr=/opt/retro-v3s --with-pkgversion='Buildroot 2018.02' --with-bugurl=http://bugs.buildroot.net/ --disable-libquadmath --disable-libsanitizer --enable-tls --enable-plugins --enable-lto --disable-libmudflap --enable-threads --without-isl --without-cloog --disable-decimal-float --with-abi=aapcs-linux --with-cpu=cortex-a7 --with-fpu=neon-vfpv4 --with-float=softfp --with-mode=arm --enable-languages=c,c++ --with-build-time-tools=/opt/retro-v3s/arm-retrov3s-linux-uclibcgnueabi/bin --enable-shared --disable-libgomp";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "abi", "aapcs-linux" }, { "cpu", "cortex-a7" }, { "float", "softfp" }, { "mode", "arm" }, { "fpu", "neon-vfpv4" }, { "tls", "gnu" } };
---
+++ x64/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/opt/retro-v3s/lib"
+XSLT_LIBS="-lxslt  -L/opt/retro-v3s/lib -lxml2 -lm -ldl -lm "
+XSLT_INCLUDEDIR="-I/opt/retro-v3s/include"
+MODULE_VERSION="xslt-1.1.29"
